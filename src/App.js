5dab5f67fa71038a2214be3272c8848b2dc7252a
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-03-30 23:37:36 +0700

    intro component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from "./components/navbar/Navbar";
 import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
