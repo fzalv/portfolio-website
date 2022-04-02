89b479a76d44bcadb9057692cae30d346b8e40e5
Muhammad Haris <kumantheas@gmail.com>
2022-04-02 12:05:25 +0700

    Works component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import Services from './components/Services/Services';
 import Experience from './components/Experience/Experience';
+import Works from './components/Works/Works';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Intro />
       <Services />
       <Experience />
+      <Works />
     </div>
   );
 }
