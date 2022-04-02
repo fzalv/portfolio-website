b188e21423950b24f2aee5a2b1ec702bf10086cd
Muhammad Haris <kumantheas@gmail.com>
2022-04-02 10:53:44 +0700

    Services card

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import './Services.css';
 import Heart from '../../img/heartemoji.png';
 import Glasses from '../../img/glasses.png';
-import Humbel from '../../img/humble.png';
-
+import Humble from '../../img/humble.png';
+import Card from "../Card/Card";
+import Resume from './resume.pdf';
 
 const Services = () => {
     return (
@@ -18,12 +19,44 @@ const Services = () => {
                     <br />
                     ispum is simpley dummy text of the printing and typesetting industry.
                 </spane>
-                <button className="button s-button">Download CV</button>
+                <a href={Resume} download>
+                    <button className="button s-button">Download CV</button>
+                </a>
                 <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
             </div>
             {/* right side */}
             <div className="cards">
-                I am the Right side
+                {/* first card */}
+                <div style={{ left: '14rem' }}>
+                    <Card
+                        emoji={Heart}
+                        heading={'Design'}
+                        detail={"Figma, Sketch, Photoshop, Adobe, Adobe xd"}
+                    />
+                </div>
+                {/* second card */}
+                <div style={{ top: "12rem", left: '-4rem' }}>
+                    <Card
+                        emoji={Glasses}
+                        heading={'Developer'}
+                        detail={"Html, Css, JavaScript, React, Nodejs, Express"}
+                    />
+                </div>
+                {/* 3rd */}
+                <div style={{ top: "19rem", left: '12rem' }}>
+                    <Card
+                        emoji={Humble}
+                        heading={"UI/UX"}
+                        detail={
+                            "Lorem ispum dummy text are usually use in section where we need some random text"
+                        }
+                        color="rgba(252, 166, 31, 0.45)"
+                    />
+                </div>
+                <div
+                    className="blur s-blur2"
+                    style={{ background: "var(--purple)" }}
+                ></div>
             </div>
         </div>
     )
