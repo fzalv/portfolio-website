4ade7a2cba799ae5adf22582d8ad0f1a6da33bcc
Muhammad Haris <kumantheas@gmail.com>
2022-04-02 08:42:48 +0700

    Floating Div

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -9,6 +9,7 @@ import Boy from '../../img/boy.png'
 import ThumbUp from '../../img/thumbup.png'
 import Crown from '../../img/crown.png'
 import Glass from '../../img/glassesimoji.png'
+import FloatingDiv from '../FloatingDiv/FloatingDiv'
 
 const Intro = () => {
     return (
@@ -31,6 +32,30 @@ const Intro = () => {
                 <img src={Vector1} alt="" />
                 <img src={Vector2} alt="" />
                 <img src={Boy} alt="" />
+                <img src={Glass} alt='' />
+                <div style={{ top: '-4%', left: '68%' }}>
+                    <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+                </div>
+                <div style={{ top: '18rem', left: '0rem' }}>
+                    <FloatingDiv image={ThumbUp} txt1='Best Design' txt2='Award'/>
+                </div>
+
+        {/* Blure Divs */}
+                <div className="blur" style={{ background: "rgb(238 210 255)" }}>
+
+                </div>
+                <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+            <div
+            className="blur"
+            style={{
+                background: "#C1F5FF",
+                top: "17rem",
+                width: "21rem",
+                height: "11rem",
+                left: "-9rem",
+            }}
+             ></div>
+
             </div>
         </div>
     )
