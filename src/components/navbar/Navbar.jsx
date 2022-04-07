65b1350d520aafae1ccb02de6cdd2cca3cc639da
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-04-07 15:59:05 +0700

    toggle component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Toggle from '../Toggle/Toggle'
 import './navbar.css'
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
         <div className='n-wrapper'>
             <div className="n-left">
                 <div className="n-name">Joni</div>
-                <span>toggle</span>
+                <Toggle />
             </div>
             <div className="n-right">
                 <div className="n-list">
